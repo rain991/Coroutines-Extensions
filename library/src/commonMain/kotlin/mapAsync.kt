73cd8c9c6f54f1e5fe04a5070ad6719a08acffa6
Savenko Ivan <savenkoivan931@gmail.com>
2026-01-29 17:49:58 +0100

    Add mapAsync function with tests

---
+++ library/src/commonMain/kotlin/mapAsync.kt
@@ -0,0 +1,31 @@
+package io.github.rain991.extensions
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+
+/**
+ * Transforms all elements of the iterable concurrently.
+ *
+ * For each element, launches a coroutine to apply the [transform] function.
+ *
+ * Example:
+ * ```
+ * val userIds = listOf(1, 2, 3)
+ *
+ * val users = userIds.mapAsync { id ->
+ *     api.getUser(id) // asynchronous call
+ * }
+ *
+ * println(users)
+ * ```
+ *
+ * Note:
+ * - One coroutine is launched per element, avoid large collections.
+ * - If any transform fails, all other coroutines will be cancelled.
+ */
+suspend fun <T, R> Iterable<T>.mapAsync(transform: suspend (T) -> R): List<R> = coroutineScope {
+    map{
+        async { transform(it) }
+    }.awaitAll()
+}
---
+++ library/src/commonTest/kotlin/MapAsyncTest.kt
@@ -0,0 +1,43 @@
+import io.github.rain991.extensions.mapAsync
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.currentTime
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MapAsyncTest {
+
+    @Test
+    fun `transforms all elements`() = runTest{
+        val list = listOf(1, 2, 3, 10)
+
+        val result = list.map { it * 2 }
+
+        assertEquals(result, listOf(2, 4, 6, 20))
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `preserves order of elements`() = runTest{
+        val list = listOf(1, 2, 3, 10)
+
+        val result = list.mapAsync {
+            delay(100)
+            if(it == 3) {
+                delay(1000)
+            }
+            it * 2
+        }
+        assertEquals(listOf(2, 4, 6, 20), result)
+        assertEquals(1100L,currentTime)
+    }
+
+    @Test
+    fun`empty list results in empty list`() = runTest{
+        val list = emptyList<Int>()
+
+        val result = list.mapAsync { it * 2 }
+        assertEquals(emptyList<Int>(), result)
+    }
+}
